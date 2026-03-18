3be9d9628205d137b6988fe41f3d637ef569f09c
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2026-03-18 12:05:31 +0530

    feat: configure navbar and hero in home.ts and declare it as a standalone component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { Hero } from '../../components/hero/hero';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [Navbar, Hero],
   templateUrl: './home.html',
 })
 export class Home {}
